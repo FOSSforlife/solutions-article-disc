882fc429fcec62f489442e96a99137f75375c5fc
Elias Jackson <ejthedj0@gmail.com>
2021-12-30 14:53:01 -0800

    Styling

--- components/PastDiscussions.tsx
+++ components/PastDiscussions.tsx
@@ -18,17 +18,17 @@ export default function PastDiscussions({ articles }: Props): ReactElement {
 	];
 
 	return (
-		<>
-			<h2 className="text-xl">Past Discussions</h2>
-			<ul className="list-disc list-inside m-4">
+		<section className="rounded-md border-2 shadow mb-2 p-2 max-w-lg mx-auto">
+			<h2 className="text-xl text-center">Past Discussions</h2>
+			<ol className="list-decimal list-inside m-4">
 				{articles.map(({ articleTitle, url, date }) => (
 					<li key={date}>
-						<a href={url}>
+						<a className="text-blue-600 visited:text-purple-600" href={url}>
 							{new Date(date).toDateString().slice(4)}: {articleTitle}
 						</a>
 					</li>
 				))}
-			</ul>
-		</>
+			</ol>
+		</section>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import PastDiscussions from '../components/PastDiscussions';
 import clientPromise from '../lib/mongodb';
+import Image from 'next/image';
+import SolutionsBlack from '../public/solutions-black.png';
+import SolutionsWhite from '../public/solutions-white.png';
+import { SocialIcon } from 'react-social-icons';
 
 export async function getServerSideProps() {
 	const db = (await clientPromise).db('articles');
-	const articles = await db.collection('articles').find({}).sort({ date: -1 }).limit(200).toArray();
-	console.log(articles);
+	const articles = await db.collection('articles').find({}).sort({ date: 1 }).limit(200).toArray();
 	return {
 		props: {
 			articles: JSON.parse(JSON.stringify(articles)),
@@ -17,27 +19,51 @@ export async function getServerSideProps() {
 
 const Home: NextPage = ({ articles }: any) => {
 	return (
-		<div className="mx-auto my-5 max-w-xl text-center">
+		<div className="mx-auto my-10 max-w-xl">
 			<Head>
 				<title>Article Discussions</title>
 				<meta name="description" content="Solutions Article Discussions" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<h1 className="text-2xl">Article Discussions</h1>
-			<p>
-				This is a weekly discussion group where we vote on an interesting article to read and have an open-ended
-				discussion on what we&apos;ve learned from it and how it applies to our lives.
-			</p>
+			<h1 className="text-3xl text-center">Article Discussions</h1>
+			<main className="text-center">
+				<p className="mt-3">
+					A weekly virtual meetup where we discuss news articles about current events and interesting ideas.
+					{/* This is a weekly discussion group where we vote on an interesting article to read and have an open-ended
+					discussion on what we&apos;ve learned from it and how it applies to our lives. */}
+				</p>
 
+				<p className="mt-4">
+					If you&apos;d like to be a part of it, join our{' '}
+					<a className="text-blue-600 visited:text-purple-600" href="https://discord.io/solutoins">
+						Discord server!
+					</a>
+				</p>
+			</main>
 			<div className="h-10"></div>
 
 			<PastDiscussions articles={articles} />
 
-			{/* TODO: Footer with some Solutions links */}
-			<footer></footer>
+			<footer className="mt-20 text-right max-w-l">
+				<div className="m-2">
+					<Image src={SolutionsBlack} layout="fixed" width={214} height={30} alt="Solutions"></Image>
+				</div>
+				<div className="m-2 space-x-2.5 text-right">
+					<SocialIcon style={{ height: 30, width: 30 }} bgColor="#777" url="https://medium.com/solutions" />
+					<SocialIcon style={{ height: 30, width: 30 }} bgColor="#777" url="https://facebook.com/solnspodcast" />
+					<SocialIcon style={{ height: 30, width: 30 }} bgColor="#777" url="https://instagram.com/solnspodcast" />
+					<SocialIcon style={{ height: 30, width: 30 }} bgColor="#777" url="https://reddit.com/r/solutionspodcast" />
+					<SocialIcon style={{ height: 30, width: 30 }} bgColor="#777" url="https://twitter.com/solnspodcast" />
+					<SocialIcon style={{ height: 30, width: 30 }} bgColor="#777" url="https://discord.io/solutions" />
+				</div>
+			</footer>
 		</div>
 	);
+	{
+		/* 1128 228 */
+		// 1070 150
+	}
 };
 
 export default Home;
